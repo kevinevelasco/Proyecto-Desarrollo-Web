3dd72b18167d10903c5fab470e88f9ef60e35ecc
kevinevelasco <90013072+kevinevelasco@users.noreply.github.com>
2024-02-20 17:47:51 -0500

    Borrar carpeta innecesaria

--- src/main/java/co/edu/javeriana/dw/proyecto/listener/JobListener.java
+++
@@ -1,8 +0,0 @@
-package co.edu.javeriana.dw.proyecto.listener;
-
-import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.batch.core.configuration.xml.JobExecutionListenerParser;
-
-public class JobListener {
-
-}
