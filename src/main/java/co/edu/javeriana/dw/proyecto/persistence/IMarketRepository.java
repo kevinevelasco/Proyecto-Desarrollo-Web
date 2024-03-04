f1f965abe040a9e8ae88cc3eca45fba8832d7a2c
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2024-03-04 01:52:08 -0500

    Se arreglan cosas de cruds de star, planet, market y product. Faltan los delete de esos y guardar cambios de planet y market

--- src/main/java/co/edu/javeriana/dw/proyecto/persistence/IMarketRepository.java
+++ src/main/java/co/edu/javeriana/dw/proyecto/persistence/IMarketRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface IMarketRepository extends JpaRepository<Market, Long> {
-    @Query("SELECT m FROM Market m WHERE LOWER(m.planet.name) LIKE LOWER(concat(:text, '%'))")
+    @Query("SELECT m FROM Market m WHERE LOWER(m.product.name) LIKE LOWER(concat(:text, '%'))")
     List<Market> findPlanetsByNameStartingWithCaseInsensitive(@Param("text") String text);
 
 }
