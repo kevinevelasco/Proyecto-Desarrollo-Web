e8df41957a307032708753c4eaec64c40c601b99
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2024-03-04 23:30:24 -0500

    Se verifica que star, market, planet y product no pueda estar vacio al momento de editar

--- src/main/java/co/edu/javeriana/dw/proyecto/model/Market.java
+++ src/main/java/co/edu/javeriana/dw/proyecto/model/Market.java
@@ -2,6 +2,7 @@ package co.edu.javeriana.dw.proyecto.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Getter
@@ -13,17 +14,34 @@ public class Market {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull(message = "La estrella no puede ser nula")
     @ManyToOne
-    @JoinColumn(name = "planet_id")
+    @JoinColumn(name = "planet_id", nullable = false)
     private Planet planet;
 
+    @NotNull(message = "La estrella no puede ser nula")
     @ManyToOne
-    @JoinColumn(name = "product_id")
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
+    @NotNull(message = "El stock no puede ser nulo")
+    @Column(nullable = false)
     private Integer stock;
+
+    @NotNull(message = "El factor demandante no puede ser nulo")
+    @Column(nullable = false)
     private Integer demandFactor;
+
+    @NotNull(message = "El factor de oferta no puede ser nulo")
+    @Column(nullable = false)
     private Integer supplyFactor;
+
+    @NotNull(message = "El precio de compra no puede ser nulo")
+    @Column(nullable = false)
     private Double buyPrice;
+
+    @NotNull(message = "El precio de venta no puede ser nulo")
+    @Column(nullable = false)
     private Double sellPrice;
 }
--- src/main/java/co/edu/javeriana/dw/proyecto/model/Planet.java
+++ src/main/java/co/edu/javeriana/dw/proyecto/model/Planet.java
@@ -1,7 +1,9 @@
 package co.edu.javeriana.dw.proyecto.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -15,22 +17,27 @@ public class Planet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "planet_name", nullable = false)
+    @NotBlank(message = "El nombre del planeta no puede ser vacio")
     private String name;
 
     @OneToMany(mappedBy = "planet")
     private List<Spacecraft> spacecrafts;
 
+    @NotNull(message = "La estrella no puede ser nula")
     @ManyToOne
+    @JoinColumn(name = "star_id", nullable = false)
     private Star star;
 
-   @ManyToMany
-   @JoinTable(
-           name = "Market",
-           joinColumns = @JoinColumn(name = "planet_id"),
-           inverseJoinColumns = @JoinColumn(name = "product_id")
-   )
-   private List<Product> products;
+    @ManyToMany
+    @JoinTable(
+            name = "Market",
+            joinColumns = @JoinColumn(name = "planet_id", nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "product_id", nullable = false)
+    )
+    private List<Product> products;
 
-   @OneToMany(mappedBy = "planet")
+    @OneToMany(mappedBy = "planet")
     private List<Market> markets;
 }
