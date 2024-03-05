bda0588cbb57bce337dc8267bdd77cb3a1422620
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-03-04 23:34:24 -0500

    Crud SpacecraftModel funcional

--- src/main/java/co/edu/javeriana/dw/proyecto/service/SpacecraftModelService.java
+++ src/main/java/co/edu/javeriana/dw/proyecto/service/SpacecraftModelService.java
@@ -1,7 +1,22 @@
 package co.edu.javeriana.dw.proyecto.service;
 
+import co.edu.javeriana.dw.proyecto.model.SpacecraftModel;
+import co.edu.javeriana.dw.proyecto.persistence.ISpacecraftModelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SpacecraftModelService {
+    @Autowired
+    private ISpacecraftModelRepository spacecraftModelRepository;
+    public List<SpacecraftModel> getAllSpacecraftModels() {return spacecraftModelRepository.findAll();}
+    public SpacecraftModel getSpacecraftModelById(Long id) {return spacecraftModelRepository.findById(id).orElse(null);}
+    public SpacecraftModel saveSpacecraftModel(SpacecraftModel spacecraftModel) {return spacecraftModelRepository.save(spacecraftModel);}
+    public void deleteSpacecraftModel(Long id) {spacecraftModelRepository.deleteById(id);}
+    public List<SpacecraftModel> buscarPorNombre(String textoBusqueda) {
+        return spacecraftModelRepository.findSpacecraftModelsByNameStartingWithCaseInsensitive(textoBusqueda);
+    }
+
 }
