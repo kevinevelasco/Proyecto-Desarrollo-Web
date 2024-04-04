e274250467b0a0688b627220d24c5479ba513b2a
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2024-04-04 17:29:33 -0500

    Arreglo el html de product, creo los componentes de angular de mis cruds

--- Proyecto-Desarrollo-Web-Client/src/app/app.module.ts
+++ Proyecto-Desarrollo-Web-Client/src/app/app.module.ts
@@ -8,13 +8,21 @@ import { FormsModule } from '@angular/forms';
 import { SpacecraftModelViewComponent } from './spacecraftModel/spacecraft-model-view/spacecraft-model-view.component';
 import { SpacecraftModelListComponent } from './spacecraftModel/spacecraft-model-list/spacecraft-model-list.component';
 import { SpacecraftModelEditComponent } from './spacecraftModel/spacecraft-model-edit/spacecraft-model-edit.component';
+import { StarComponent } from './star/star.component';
+import { StarListComponent } from './star/star-list/star-list.component';
+import { StarEditComponent } from './star/star-edit/star-edit.component';
+import { StarViewComponent } from './star/star-view/star-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpacecraftModelViewComponent,
     SpacecraftModelListComponent,
-    SpacecraftModelEditComponent
+    SpacecraftModelEditComponent,
+    StarComponent,
+    StarListComponent,
+    StarEditComponent,
+    StarViewComponent
   ],
   imports: [
     BrowserModule,
