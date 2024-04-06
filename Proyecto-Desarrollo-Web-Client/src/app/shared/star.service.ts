77e6193fd321ec122a41ded37bfb4bedd7a54b8a
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-04-06 18:51:20 -0500

    Empezando con three js

--- Proyecto-Desarrollo-Web-Client/src/app/shared/star.service.ts
+++ Proyecto-Desarrollo-Web-Client/src/app/shared/star.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Star } from '../model/star';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarService {
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  private headers = new HttpHeaders(
+    {
+      'Content-Type': 'application/json'
+      //'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+  )
+
+  getStars(): Observable<Star[]> {
+    return this.http.get<Star[]>(`${environment.serverUrl}/api/star/list`);
+  }
+  getStar(id: number): Observable<Star>  {
+    return this.http.get<Star>(`${environment.serverUrl}/api/star/${id}`);
+  }
 }
