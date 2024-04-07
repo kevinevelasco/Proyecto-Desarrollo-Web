c9033cbce1df898378501600ffe5c82f6c0a0955
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-04-07 11:13:35 -0500

    Conexión de log in entre componentes realizada

--- Proyecto-Desarrollo-Web-Client/src/app/space-travel/space-travel.component.ts
+++ Proyecto-Desarrollo-Web-Client/src/app/space-travel/space-travel.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Player } from '../model/player';
+import { Subscription } from 'rxjs';
+import { LoginService } from '../services/auth/login.service';
 
 @Component({
   selector: 'app-space-travel',
   templateUrl: './space-travel.component.html',
-  styleUrl: './space-travel.component.css'
+  styleUrls: ['./space-travel.component.css']
 })
-export class SpaceTravelComponent {
+export class SpaceTravelComponent implements OnInit, OnDestroy {
+  userLoginOn: boolean = false;
+  userData?: Player;
+  private loginSubscription: Subscription;
+  private userDataSubscription: Subscription;
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.loginSubscription = this.loginService.currentUserLoginOn.subscribe({
+      next: (userLoginOn) => {
+        this.userLoginOn = userLoginOn;
+      }
+    });
+
+    this.userDataSubscription = this.loginService.currentUserData.subscribe({
+      next: (userData) => {
+        this.userData = userData;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
+    if (this.userDataSubscription) {
+      this.userDataSubscription.unsubscribe();
+    }
+  }
 }
