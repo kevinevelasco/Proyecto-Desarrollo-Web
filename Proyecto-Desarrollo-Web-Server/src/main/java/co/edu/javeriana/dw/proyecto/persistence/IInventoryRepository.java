16d6ba2f0c47a54238a5e6668157a4182f36aa61
Nicolas Padilla <npadilla133@gmail.com>
2024-04-07 03:02:41 -0500

    Controllers de Inventory

--- Proyecto-Desarrollo-Web-Server/src/main/java/co/edu/javeriana/dw/proyecto/persistence/IInventoryRepository.java
+++ Proyecto-Desarrollo-Web-Server/src/main/java/co/edu/javeriana/dw/proyecto/persistence/IInventoryRepository.java
@@ -1,8 +1,14 @@
 package co.edu.javeriana.dw.proyecto.persistence;
 
 import co.edu.javeriana.dw.proyecto.model.Inventory;
-import co.edu.javeriana.dw.proyecto.model.Market;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IInventoryRepository extends JpaRepository<Inventory, Long> {
+
+    @Query("SELECT i FROM Inventory i WHERE i.spacecraft.id = ?1")
+    List<Inventory> findAllBySpacecraftId(Long spacecraft);
 }
