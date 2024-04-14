5b22b8a9572be907c61f1ded6760b0417d63f93d
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-04-14 11:36:43 -0500

    css final space-travel

--- Proyecto-Desarrollo-Web-Client/src/app/space-travel/ui/ui.component.ts
+++ Proyecto-Desarrollo-Web-Client/src/app/space-travel/ui/ui.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 import { SpacecraftService } from '../../services/spacecraft.service';
 import { SpacecraftPlanet } from './spacecraftPlanet';
 import { delay } from 'rxjs/operators';
+import { MarketService } from '../../services/market.service';
+import { Market } from '../../model/market';
 
 @Component({
   selector: 'app-ui',
@@ -19,7 +21,8 @@ export class UiComponent implements OnInit, OnDestroy {
   constructor(
     private engineService: EngineService,
     private router: Router,
-    private spacecraftService: SpacecraftService
+    private spacecraftService: SpacecraftService,
+    private marketService: MarketService
   ) {}
 
   @Input() userData?: Player;
@@ -31,23 +34,26 @@ export class UiComponent implements OnInit, OnDestroy {
   currentPlanet: Planet;
   deployPlanetList: boolean = false;
   starClicked: Star;
+  marketData: Market[]= [];
 
   ngOnInit(): void {
     console.log('user info', this.userData);
     console.log(this.currentStar);
     console.log(this.starPlanets);
     console.log(this.nearestStars);
-    this.engineService.getClicInPlanetObservable().subscribe(planeta => {
+    this.engineService.getClicInPlanetObservable().subscribe((planeta) => {
       this.currentPlanet = planeta;
-      console.log("el planeta clickeado fue", this.currentPlanet);
+      console.log('el planeta clickeado fue', this.currentPlanet);
       this.booleanPlanet = !this.booleanPlanet;
     });
     if (this.userData && this.userData.spacecraft) {
-    this.spacecraftService.getPlanetBySpacecraft(this.userData?.spacecraft.id).subscribe((planet: Planet) => {
-      console.log('El planeta es:', planet.name);
-      this.inPlanet = planet;
-    });
-    this.userData.spacecraft.planet = this.inPlanet;
+      this.spacecraftService
+        .getPlanetBySpacecraft(this.userData?.spacecraft.id)
+        .subscribe((planet: Planet) => {
+          console.log('El planeta es:', planet.name);
+          this.inPlanet = planet;
+        });
+      this.userData.spacecraft.planet = this.inPlanet;
     }
   }
 
@@ -58,43 +64,58 @@ export class UiComponent implements OnInit, OnDestroy {
     this.engineService.showPlanet(planet);
     this.booleanPlanet = true;
     this.currentPlanet = planet;
+    this.marketService.getMarketsByPlanetId(this.currentPlanet.id)
+    .subscribe((markets: Market[]) => {
+            this.marketData = markets;
+            // this.currentPlanet = this.marketData.
+            console.log("MarketData:", this.marketData);
+      });
   }
   onCancelClick() {
     this.booleanPlanet = false;
     this.engineService.setInfo(false);
   }
   onTravelClick(currentPlanet: Planet) {
-    if(this.userData != null && this.userData != undefined){
+    if (this.userData != null && this.userData != undefined) {
       this.onOtherStarPlanetClick(currentPlanet, this.userData, true);
     }
-    
-    }
-  onOtherStarPlanetClick(planet: Planet, userData: Player, viajarAPlaneta : boolean) {
-    //convertimos la respuesta en la interfaz SpacecraftPlanet
-    const spacecraftPlanet: SpacecraftPlanet = {idPlanet: planet.id, idUser: userData.id};
-
-    this.spacecraftService.setPlanet(spacecraftPlanet).pipe(
-      //delay(2000)
-    ).subscribe(data => {
-      console.log(data);
-      if(viajarAPlaneta){
-        this.router.navigate(['/space-travelling']).then(() => {
-          // Redirigir a '/space-travel' después de 2 segundos
-          setTimeout(() => {
-            this.router.navigate(['/sell']); 
-          }, 2000); // retraso de 2 segundos
-        });
-      } else{
-        this.router.navigate(['/space-travelling']).then(() => {
-          // Redirigir a '/space-travel' después de 5 segundos
-          setTimeout(() => {
-            this.router.navigate(['/space-travel']); 
-          }, 5000); // retraso de 5 segundos
-        });
-      }
-    });
   }
-  
+  onOtherStarPlanetClick(
+    planet: Planet,
+    userData: Player,
+    viajarAPlaneta: boolean
+  ) {
+    //convertimos la respuesta en la interfaz SpacecraftPlanet
+    const spacecraftPlanet: SpacecraftPlanet = {
+      idPlanet: planet.id,
+      idUser: userData.id,
+    };
+
+    this.spacecraftService
+      .setPlanet(spacecraftPlanet)
+      .pipe
+      //delay(2000)
+      ()
+      .subscribe((data) => {
+        console.log(data);
+        if (viajarAPlaneta) {
+          this.router.navigate(['/space-travelling']).then(() => {
+            // Redirigir a '/space-travel' después de 2 segundos
+            setTimeout(() => {
+              this.router.navigate(['/sell']);
+            }, 2000); // retraso de 2 segundos
+          });
+        } else {
+          this.router.navigate(['/space-travelling']).then(() => {
+            // Redirigir a '/space-travel' después de 5 segundos
+            setTimeout(() => {
+              this.router.navigate(['/space-travel']);
+            }, 5000); // retraso de 5 segundos
+          });
+        }
+      });
+  }
+
   onStarClick(star: Star) {
     console.log('star clickeada', star);
     if (this.starClicked === star) {
@@ -103,5 +124,5 @@ export class UiComponent implements OnInit, OnDestroy {
       this.starClicked = star; // Almacena la estrella clicada
       this.deployPlanetList = true; // Abre la lista de planetas para la nueva estrella
     }
-    }
+  }
 }
